8e096084f53d8d30329056526fbda844fdc3555c
akexinad <danixeka@gmail.com>
2021-03-09 18:41:49 +1100

    login mutation and error handling

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,7 +1,15 @@
 import argon2 from "argon2";
-import { User } from "../entites/User";
 import { MyContext } from "src/types";
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import {
+    Arg,
+    Ctx,
+    Field,
+    InputType,
+    Mutation,
+    ObjectType,
+    Resolver
+} from "type-graphql";
+import { User } from "../entites/User";
 
 @InputType()
 class UsernamePasswordInput {
@@ -9,6 +17,24 @@ class UsernamePasswordInput {
     username: string;
     @Field()
     password: string;
+}
+
+@ObjectType()
+class FieldError {
+    @Field()
+    field: string;
+
+    @Field()
+    message: string;
+}
+
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], { nullable: true })
+    errors?: FieldError[];
+
+    @Field(() => User, { nullable: true })
+    user?: User;
 }
 
 @Resolver()
@@ -28,5 +54,46 @@ export class UserResolver {
         await ctx.em.persistAndFlush(user);
 
         return user;
+    }
+
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg("options") options: UsernamePasswordInput,
+        @Ctx() ctx: MyContext
+    ): Promise<UserResponse> {
+        const userExists = await ctx.em.findOne(User, {
+            username: options.username.toLowerCase()
+        });
+
+        if (!userExists) {
+            return {
+                errors: [
+                    {
+                        field: "Username",
+                        message: "Invalid login"
+                    }
+                ]
+            };
+        }
+
+        const valid = await argon2.verify(
+            userExists.password,
+            options.password
+        );
+
+        if (!valid) {
+            return {
+                errors: [
+                    {
+                        field: "Password",
+                        message: "Invalid login"
+                    }
+                ]
+            };
+        }
+
+        return {
+            user: userExists
+        };
     }
 }
