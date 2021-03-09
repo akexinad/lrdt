ff4d3d2bed62f8815878aab9020496a9d8c28966
akexinad <danixeka@gmail.com>
2021-03-09 13:37:47 +1100

    setup server with graphql and express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,31 @@
 import { MikroORM } from "@mikro-orm/core";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { buildSchema } from "type-graphql";
 import { __prod__ } from "./constants";
-import { Post } from "./entites/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
-    
     const orm = await MikroORM.init(mikroOrmConfig);
 
     // automatically run the migrations
-    orm.getMigrator().up();
+    await orm.getMigrator().up();
 
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    });
+
+    apolloServer.applyMiddleware({ app })
+
+    app.listen(5000, () => {
+        console.log("listening on http://localhost:5000/graphql");
+    });
 };
 
 main().catch((err) => console.log(err));
