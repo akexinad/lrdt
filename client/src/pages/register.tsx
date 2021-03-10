4c6344ea8e183b97d72c88184bef9963ae1bd21e
akexinad <danixeka@gmail.com>
2021-03-10 21:43:25 +1100

    error handling on the client

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -5,18 +5,30 @@ import { InputField } from "../components/InputField";
 import { Wrapper } from "../components/Wrapper";
 import { useRegisterMutation } from "../generated/graphql";
 import { initialFormValues } from "../utils/formUtils";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
 
 type RegisterProps = {};
 
 const Register: FC<RegisterProps> = () => {
+    const router = useRouter();
+
     const [, register] = useRegisterMutation();
 
     return (
         <Wrapper variant="small">
             <Formik
                 initialValues={initialFormValues}
-                onSubmit={async (values) => {
+                onSubmit={async (values, { setErrors }) => {
                     const response = await register(values);
+
+                    if (response.data.register.errors) {
+                        return setErrors(
+                            toErrorMap(response.data.register.errors)
+                        );
+                    }
+
+                    router.push("/");
                 }}
             >
                 {({ values, handleChange, isSubmitting }) => (
