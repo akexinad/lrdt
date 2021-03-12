b36586bb028b4339e648735e1781275b5b450c83
akexinad <danixeka@gmail.com>
2021-03-12 15:25:21 +1100

    moved utils folder

---
+++ server/src/utils/validateRegister.ts
@@ -0,0 +1,42 @@
+import { UsernamePasswordEmailInput } from "src/resolvers/usernamePasswordEmailInput";
+
+export const validateRegister = (options: UsernamePasswordEmailInput) => {
+
+    if (options.username.includes("@")) {
+        return [
+            {
+                field: "username",
+                message: "Username cannot contain @ sign"
+            }
+        ];
+    }
+
+    if (options.username.length < 6) {
+        return [
+            {
+                field: "username",
+                message: "Username must be at least six characters"
+            }
+        ];
+    }
+
+    if (!options.email.includes("@")) {
+        return [
+            {
+                field: "email",
+                message: "Invalid email"
+            }
+        ];
+    }
+
+    if (options.password.length < 6) {
+        return [
+            {
+                field: "password",
+                message: "Password must be at least six characters"
+            }
+        ];
+    }
+
+    return null;
+};
